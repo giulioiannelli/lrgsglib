4e93de166052c53408c103f10f2f7f715b6974dd
giulioiannelli <giulioiannelli.w@gmail.com>
2025-10-14 19:55:13 +0200

    Add MultispectralGraph module with generators and default parameters

--- src/lrgsglib/nx_patches/__init__.py
+++ src/lrgsglib/nx_patches/__init__.py
@@ -5,4 +5,5 @@ from .HofieldNN import HofieldNN
 from .DGMgraph import DGMgraph
 from .Lattice2D import Lattice2D, load_or_compute_Lattice2D
 from .Lattice3D import Lattice3D, load_or_compute_Lattice3D
+from .MultispectralGraph import MultispectralGraph
 from .funcs import *
