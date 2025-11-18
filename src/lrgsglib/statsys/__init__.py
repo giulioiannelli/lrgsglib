11df51e24a32bad44ccfba4cc1789f6ddf0afe57
giulio <109902216+giulioiannelli@users.noreply.github.com>
2025-11-18 16:07:33 +0100

    Refactor contact process variants

--- src/lrgsglib/statsys/__init__.py
+++ src/lrgsglib/statsys/__init__.py
@@ -1,5 +1,5 @@
 from .BinDynSys import BinDynSys
 from .IsingDynamics import IsingDynamics
-from .ContactProcess import ContactProcess
+from .ContactProcess import ContactProcess, ContactProcessBase, ContactProcessEI, ContactProcessSIR
 from .SignedRW import SignedRW
 from .VoterModel import VoterModel
